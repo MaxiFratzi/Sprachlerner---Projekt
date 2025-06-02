8740cde10f1952729f46e121448ae5a28441c159
Floooooooooooooooooooo <fkainz@tsn.at>
2025-06-02 09:01:03 +0200

    feat: add scripts for database backup and restoration

---
+++ restoredb.bat
@@ -0,0 +1,2 @@
+C:\xampp\mysql\bin\mysql -u root vokabeln < C:\xampp\htdocs\3BHWII\Vokabeltrainer\Sprachlerner---Projekt\Sprachentrainer.sql
+pause
---
+++ savedb.bat
@@ -0,0 +1,2 @@
+C:\xampp\mysql\bin\mysqldump -u root vokabeln > C:\xampp\htdocs\3BHWII\Vokabeltrainer\Sprachlerner---Projekt\Sprachentrainer.sql
+pause
